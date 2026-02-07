8b9848e54e53c579c70a508c0fd1df82a4864595
josemicode <jmiguelojedagutierrez@gmail.com>
2026-02-07 18:40:13 +0100

    Restored old commit (pre-aritsts-page)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "@/utils/trpc";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "@/server";
-import Navbar from "@/app/components/Navbar";
-import Footer from "@/app/components/Footer";
 
 export default function RootLayout({
     children,
@@ -27,25 +25,12 @@ export default function RootLayout({
 
     return (
         <html lang="en">
-            <body
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    minHeight: "100vh",
-                }}
-            >
-                <Navbar />
-                <div style={{ flex: 1 }}>
-                    <trpc.Provider
-                        client={trpcClient}
-                        queryClient={queryClient}
-                    >
-                        <QueryClientProvider client={queryClient}>
-                            {children}
-                        </QueryClientProvider>
-                    </trpc.Provider>
-                </div>
-                <Footer />
+            <body>
+                <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                    <QueryClientProvider client={queryClient}>
+                        {children}
+                    </QueryClientProvider>
+                </trpc.Provider>
             </body>
         </html>
     );
